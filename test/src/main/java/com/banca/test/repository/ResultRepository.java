76dae25586a3f0adaa25c66d39674012fac4822b
reyniercs90 <reyniercs90@gmail.com>
2025-04-08 04:53:23 -0300

    Deleting file from main branch

--- test/src/main/java/com/banca/test/repository/ResultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.banca.test.repository;
-
-import com.banca.test.entities.ResultEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResultRepository extends JpaRepository<ResultEntity,Long> {
-}
